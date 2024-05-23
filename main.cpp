343b816b93763b33463afc41cfdba6ed3ccee2da
Younes <77817768+Younesdev12@users.noreply.github.com>
2024-05-23 22:04:37 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,30 +3,30 @@
 #include <string>
 #include <string.h>
 
-// Global Variables
-HINSTANCE hInstance;         //the current instance
-const char* WindowTitle;     //The title of the window
-const char* WindowClassName; //the class name
 
-LRESULT    CALLBACK    WndProc(HWND, UINT, WPARAM, LPARAM); //Used for getting messages and stuff from e.g, moving your mouse
-WNDCLASS               RegisterWindowClass(HINSTANCE); //registers the class in windows so you can show it
-BOOL                   InitInstance(HINSTANCE, int);  //initialises the class and window
+HINSTANCE hInstance;         
+const char* WindowTitle;     
+const char* WindowClassName; 
 
-void                   InitVariables(); //used to declare the window title, class name, etc.
+LRESULT    CALLBACK    WndProc(HWND, UINT, WPARAM, LPARAM);
+WNDCLASS               RegisterWindowClass(HINSTANCE);
+BOOL                   InitInstance(HINSTANCE, int);
+
+void                   InitVariables();
 
 #define DefaultWindowStyle (WS_POPUP | WS_HSCROLL | WS_SYSMENU | WS_CAPTION| WS_SYSMENU | WS_THICKFRAME | WS_MINIMIZEBOX |WS_TABSTOP| WS_MAXIMIZEBOX)
 
-int WINAPI WinMain(HINSTANCE currentInstance, HINSTANCE prevInstance /*this is always null*/, PSTR cmdLineArgs, INT cmdShow)
+int WINAPI WinMain(HINSTANCE currentInstance, HINSTANCE prevInstance , PSTR cmdLineArgs, INT cmdShow)
 {
 	InitVariables();
 
-	//the & fetches the address of the RegisterWindowClass "variable" so it can be pointed to. cpp magic basically
+	
 
 	if (!InitInstance(hInstance, cmdShow))
 		return FALSE;
 
 	MSG msg{};
-	//this will be running forever until the window is closed. GetMessage constantly loops getting system messages like mouse moves, etc
+	
 	while (GetMessageA(&msg, nullptr, 0, 0))
 	{
 		TranslateMessage(&msg);
@@ -36,7 +36,7 @@ int WINAPI WinMain(HINSTANCE currentInstance, HINSTANCE prevInstance /*this is a
 	return 0;
 }
 
-//Setting up the variable init thing
+
 void InitVariables()
 {
 	WindowTitle     = "A title here";
@@ -47,8 +47,8 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam)
 {
 	switch (msg)
 	{
-	case WM_DESTROY: //clicking the X button on the window basically
-		PostQuitMessage(0); //tells the program to exit
+	case WM_DESTROY:
+		PostQuitMessage(0);
 		break;
 	default:
 		return DefWindowProcA(hWnd, msg, wParam, lParam);
@@ -69,21 +69,21 @@ WNDCLASS RegisterWindowClass(HINSTANCE instance)
 
 BOOL InitInstance(HINSTANCE instance, int cmdShow)
 {
-	hInstance = instance; //stores the global instance as this method's instance
+	hInstance = instance;
 
 	HWND hWnd =
 		CreateWindowA(
-			WindowClassName, WindowTitle,     //Gives it the class and title
-			DefaultWindowStyle,               //Window style
-			CW_USEDEFAULT, CW_USEDEFAULT,     //initial window position
-			900 /*width*/, 450/*height*/,
+			WindowClassName, WindowTitle,   
+			DefaultWindowStyle,               
+			CW_USEDEFAULT, CW_USEDEFAULT,     
+			900 , 450,
 			nullptr, nullptr, hInstance, nullptr
 		);
 
 	if (!hWnd)
-		return FALSE; //same as 0
+		return FALSE;
 
-	ShowWindow(hWnd, cmdShow); //actually shows the window on your screen
+	ShowWindow(hWnd, cmdShow);
 	UpdateWindow(hWnd);
-	return TRUE; //lets other code know it's shown correctly
+	return TRUE;
 }
